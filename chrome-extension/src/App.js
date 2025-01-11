067e003db3a648fb50f1ec9b7fca1bd90b4ce060
Sophia Ma <68702931+soapjia@users.noreply.github.com>
2025-01-11 16:49:42 -0500

    I MADE THE SCREEN!!

--- chrome-extension/src/App.js
+++ chrome-extension/src/App.js
@@ -1,21 +1,55 @@
 import React from 'react';
+import './App.css';
+import duckImage from './duck.jpg';
 
 function App() {
+
+  // When user clicks on a circle
+  const handleCircleClick = (circleId) => {
+    console.log(`Circle ${circleId} clicked`);
+    // can add another things here
+    console.log("clicked");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "App">
+      <div className="title">Who are you talking to today?</div>
+      <div className="circle-container">
+        <div className="row">
+          <div className="circle-wrapper" onClick={() => handleCircleClick(1)}>
+            <div className="circle" style={{ backgroundImage: `url(${duckImage})`}}></div>
+            <div className="name">cynthia</div>
+            <div className="role">student</div>
+          </div>
+          <div className="circle-wrapper" onClick={() => handleCircleClick(2)}>
+          <div className="circle" style={{ backgroundImage: `url(${duckImage})`}}></div>
+            <div className="name">grass</div>
+            <div className="role">student</div>
+          </div>
+          <div className="circle-wrapper" onClick={() => handleCircleClick(3)}>
+          <div className="circle" style={{ backgroundImage: `url(${duckImage})`}}></div>
+            <div className="name">joy</div>
+            <div className="role">student</div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="circle-wrapper" onClick={() => handleCircleClick(4)}>
+          <div className="circle" style={{ backgroundImage: `url(${duckImage})`}}></div>
+            <div className="name">cynthia1</div>
+            <div className="role">student</div>
+          </div>
+          <div className="circle-wrapper" onClick={() => handleCircleClick(5)}>
+          <div className="circle" style={{ backgroundImage: `url(${duckImage})`}}></div>
+            <div className="name">grass2</div>
+            <div className="role">student</div>
+          </div>
+          <div className="circle-wrapper" onClick={() => handleCircleClick(6)}>
+          <div className="circle" style={{ backgroundImage: `url(${duckImage})`}}></div>
+            <div className="name">joy3</div>
+            <div className="role">student</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
