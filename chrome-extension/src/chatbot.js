92128c3f26be16e9296e3566cd146073d4ca3ba1
cynthia2385 <153042927+cynthia2385@users.noreply.github.com>
2025-01-12 05:25:24 -0500

    deleted comments

--- chrome-extension/src/chatbot.js
+++ chrome-extension/src/chatbot.js
@@ -3,7 +3,6 @@ import duckImage from './duck.jpg';
 
 const Chatbot = () => {
   useEffect(() => {
-    // Function to dynamically load the Voiceflow chatbot script
     const loadChatbotScript = () => {
       const script = document.createElement('script');
       script.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
@@ -16,13 +15,9 @@ const Chatbot = () => {
             versionID: 'production',
             render: {
               mode: 'embedded',
-              target: document.getElementById('flat-chat'), // Ensure this element exists
+              target: document.getElementById('flat-chat'),
             },
             assistant: {
-              //title: "Speak your Dev!",
-              //description: "How can I help you today?",
-              //image: duckImage,
-              // color: "#dafefe",
               stylesheet: "https://github.com/soapjia/talkdevtome/blob/main/chrome-extension/public/chatbot.css" //link to your self-hosted stylesheet
             },
             autostart: true,
@@ -41,18 +36,16 @@ const Chatbot = () => {
 
     loadChatbotScript();
 
-    // Clean up the script when the component is unmounted
     return () => {
       const script = document.querySelector('script[src="https://cdn.voiceflow.com/widget/bundle.mjs"]');
       if (script) {
         script.remove();
       }
     };
-  }, []); // Empty array ensures this only runs once when the component mounts
+  }, []);
 
   return (
     <div id="chatbot-container">
-      {/* Add target element for rendering */}
       <div id="flat-chat"></div>
     </div>
   );
